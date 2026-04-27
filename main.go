387afc64473fbf979788b3d183655d26794e1657
Benjamin Kleyner <benkleyner@gmail.com>
2026-04-26 21:38:44 -0400

    basic cli

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const version = "0.1.0"
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "hello":
+		fmt.Println("hello from dev-sync")
+	case "version":
+		fmt.Println(version)
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n\n", os.Args[1])
+		usage()
+		os.Exit(1)
+	}
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: dev-sync <command>")
+	fmt.Fprintln(os.Stderr, "")
+	fmt.Fprintln(os.Stderr, "commands:")
+	fmt.Fprintln(os.Stderr, " hello     print a greeting")
+	fmt.Fprintln(os.Stderr, " version   print the version")
+}
